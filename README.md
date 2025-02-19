c35c4435db3827b677df2cf10f12cccad2517bd0
Jaroslav Pavlicek <all@pavrda.cz>
2025-02-19 17:44:00 +0100

    Upraveno propojeni stranek

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # sablony
+
+TODO:
+
+Homepage
+* aktualizovat texty u popisu služeb
+* vyměnit Media Room za Calmio nebo Zaleť si
+* odkaz na "Napište nám hned" zapojit na kontakt
+* vybrat vhodná loga a seřadit je
+* připravit API pro přihlášení newsletteru
+* odkaz v patičce "Napište nám hned" zapojit na kontakt
+* doplnit odkazy do patičky
+
+Menu:
+* přidat rozbalení služeb, když najedu na "Naše služby"
+* přidat styl pro právě vybranou stránku
+* tlačítko Navázat spolupráci - co s ním?
+
+Naše služby
+* zapojit odkazy na stránky jednotlivých služeb
+* upravit ikonky u jednotlivých služeb
+
+Reference:
+* aktualizovat text v nadpisu
+* připravit API pro načítání dalších
+* připravit jinou grafiku po 5 řádcích, ať to není stále stejné a únavné
+* připravit filtr, vyhledávání
+* připravit text, grafiku, ... pro každou z aplikací
+* doplnit text vedle Jany
+
+O nás:
+* napsat kompletně texty
+
+Kontakt:
+* napojit formulář
+* doplnit texty
+
+Blog:
+* napojit na API
+* vyměnit logo
+
+Blog detail:
+* napojit na API
+
+
--- js/tailwind.config.js
+++ js/tailwind.config.js
@@ -1,49 +1,47 @@
-
 tailwind.config = {
-    theme: {
+  theme: {
     extend: {
-    colors: {
-    green: '#da373d',
-    blue: '#0187FF',
-    orange: "#F0953F",
-    lightBackground: "#9EA6B3",
-    tag: '#B3BAC6',
-    text: "#B3BAC6",
-    blueBackground: "#304352",
-    borderGray: "#E8EAEE",
-    backgroundGrayLight: "#F1F2F4",
-    backgroundGray: "#E5EAF2",
-    grayBlue: "#636973",
-    textOpacity: "rgba(179,186,198,0.3)",
-    background: "#1D1F22",
-},
-    backgroundImage: {
-    gradientText: "linear-gradient(90deg, #ff8f20 0%, #ff3232 100%)",
-    gradientBox: "linear-gradient(199deg, #8DA1C3 0%, #B3CEFA 100%)",
-    gradientBlack: "linear-gradient(183deg, #333942 0%, #3B424D 100%)",
-    gradientActive: "linear-gradient(199deg, #8DA1C3 0%, #B3CEFA 100%)",
-    radialMarker: "radial-gradient(closest-side at 50% 50%, #4B93FF30 0%, #1D1F2230 100%)",
-    heroMobile: "url('./images/hero/hero.webp')",
-    heroService: "url('./icons/services-detail/wireframe.svg')",
-    peopleJaroslav: "url('./images/people/jaroslav.webp')",
-    peopleJana: "url('./images/people/jana.webp')",
+      colors: {
+        green: "#da373d",
+        blue: "#0187FF",
+        orange: "#F0953F",
+        lightBackground: "#9EA6B3",
+        tag: "#B3BAC6",
+        text: "#B3BAC6",
+        blueBackground: "#304352",
+        borderGray: "#E8EAEE",
+        backgroundGrayLight: "#F1F2F4",
+        backgroundGray: "#E5EAF2",
+        grayBlue: "#636973",
+        textOpacity: "rgba(179,186,198,0.3)",
+        background: "#1D1F22",
+      },
+      backgroundImage: {
+        gradientText: "linear-gradient(90deg, #ff8f20 0%, #ff3232 100%)",
+        gradientBox: "linear-gradient(199deg, #8DA1C3 0%, #B3CEFA 100%)",
+        gradientBlack: "linear-gradient(183deg, #333942 0%, #3B424D 100%)",
+        gradientActive: "linear-gradient(199deg, #8DA1C3 0%, #B3CEFA 100%)",
+        radialMarker:
+          "radial-gradient(closest-side at 50% 50%, #4B93FF30 0%, #1D1F2230 100%)",
+        heroMobile: "url('./images/hero/hero.webp')",
+        heroService: "url('./icons/services-detail/wireframe.svg')",
+        peopleJaroslav: "url('./images/people/jaroslav.webp')",
+        peopleJana: "url('./images/people/jana.webp')",
         check: "url('./icons/check.svg')",
-
+      },
+      boxShadow: {
+        primaryButton: "0px 8px 25px #0187FF30",
+        card: "0px 12px 50px #1D1F221A;",
+      },
+      keyframes: {
+        bounceSlow: {
+          "0%, 100%": { transform: "translateY(-10px)" },
+          "50%": { transform: "translateY(20px)" },
+        },
+      },
+      animation: {
+        "bounce-slow": "bounceSlow 2s ease-in-out infinite",
+      },
     },
-    boxShadow: {
-        primaryButton: '0px 8px 25px #0187FF30',
-        card: "0px 12px 50px #1D1F221A;"
-    },
-    keyframes: {
-    bounceSlow: {
-    '0%, 100%': { transform: 'translateY(-10px)' },
-    '50%': { transform: 'translateY(20px)' },
-},
-},
-    animation: {
-    'bounce-slow': 'bounceSlow 2s ease-in-out infinite',
-
-},
-}
-}
-}
+  },
+};
